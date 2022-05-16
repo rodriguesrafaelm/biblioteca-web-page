8bfdf7d6126fe486af0653bcc87ccad2279d6b8e
Rafael <79672000+rodriguesrafaelm@users.noreply.github.com>
2022-05-16 12:42:06 -0300

    Image URL

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -21,7 +21,7 @@ form.addEventListener('submit', recebeDados)
             document.getElementById('nome-autor').innerHTML = `Autor: ${data1.autor}`;
             document.getElementById('ano-publicado').innerHTML = `Ano de publicação: ${data1.ano_publicado}`;
             document.getElementById('editora').innerHTML = `Editora: ${data1.editora}`;
-            document.getElementById('book-image').innerHTML = `URL da imagem: ${data1.image_url}` + '<img src="${data1.image_url}"></img>';
+            document.getElementById('book-image').innerHTML = `URL da imagem: ${data1.image_url}` + `<img src="${data1.image_url}"></img>`;
 }
 
 }
